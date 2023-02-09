cf3c91f89c72ba81c2731ea00db6105d8116758c
Matthew McCall <matthew+github@mmccall.dev>
2023-02-09 11:20:03 -0500

    kokkos in tree build test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
+#include "Kokkos_Core.hpp"
+#include "Kokkos_Sort.hpp"
+
 #include <iostream>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    // Simple kokkos for each
+    Kokkos::parallel_for("HelloWorld", 10, KOKKOS_LAMBDA (const int i) {
+        std::cout << "Hello, World! " << i << std::endl;
+    });
+
+    // Simple kokkos sort
+    Kokkos::View<int*> a("a", 10);
+    Kokkos::parallel_for("Init", 10, KOKKOS_LAMBDA (const int i) {
+        a(i) = 10 - i;
+    });
+
+    Kokkos::sort(a);
+
+    // Kokkos array of pairs
+    Kokkos::View<Kokkos::pair<int, int>*> b("b", 10);
+
+    // Sort b with Kokkos::Experimental::sort
+
     return 0;
 }
