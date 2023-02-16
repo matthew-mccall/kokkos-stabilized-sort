8fb175ccaea7d5c10899e7d62eb2d5054f128782
Matthew McCall <matthew+github@mmccall.dev>
2023-02-16 10:27:42 -0500

    Demonstrate instability of existing sorting implementation

--- kokkos_unstable_sort/main.cpp
+++ kokkos_unstable_sort/main.cpp
@@ -10,7 +10,13 @@
 template <typename ViewType>
 void print_view(ViewType view) {
     for (int i = 0; i < view.extent(0); i++) {
-        std::cout << view(i).first << " " << view(i).second << std::endl;
+        std::cout << '(' << view(i).first << " " << view(i).second << ')';
+
+        if (i < view.extent(0) - 1) {
+            std::cout << ", ";
+        } else {
+            std::cout << std::endl;
+        }
     }
 }
 
@@ -23,12 +29,12 @@ struct custom_comparator {
 };
 
 int main() {
-    Kokkos::initialize();
+    Kokkos::ScopeGuard kokkos;
 
     // Kokkos generate array of pairs
-    Kokkos::View<Kokkos::pair<int, int>*> b("b", 10);
-    Kokkos::parallel_for("Init", 10, KOKKOS_LAMBDA (const int i) {
-        b(i) = Kokkos::pair<int, int>(10 - i, i);
+    Kokkos::View<Kokkos::pair<int, int>*> b("b", 30);
+    Kokkos::parallel_for("Init", 30, KOKKOS_LAMBDA (const int i) {
+        b(i) = Kokkos::pair<int, int>((29 - i) / 3, (i % 3) + 1);
     });
 
     std::cout << "Before sort" << std::endl;
@@ -37,7 +43,7 @@ int main() {
     //=== Kokkos nested sort by key with sort_team ===
 
     // Create a team policy
-    Kokkos::TeamPolicy<> policy(10, 1);
+    Kokkos::TeamPolicy<> policy(30, 1);
 
     //=== Kokkos nested sort with custom comparator ===
     Kokkos::parallel_for("Sort", policy, KOKKOS_LAMBDA (const Kokkos::TeamPolicy<>::member_type& team) {
