926f6dad109d38b0a26302c41f15bea0197be2ec
Matthew McCall <matthew+github@mmccall.dev>
2023-02-23 13:24:51 -0500

    Use view for vector of pairs

--- kokkos_stabilized_unstable_sort/main.cpp
+++ kokkos_stabilized_unstable_sort/main.cpp
@@ -10,17 +10,20 @@
 #include "Kokkos_Core.hpp"
 #include "Kokkos_NestedSort.hpp"
 
-template<typename T>
-void print_vector(std::vector<std::pair<std::uint32_t, std::uint32_t>> &a, Kokkos::View<T*> view) {
-    // Deep copy to host
-    Kokkos::View<T*, Kokkos::HostSpace> host_view("host_view", view.extent(0));
-    Kokkos::deep_copy(host_view, view);
+template<typename T, typename U>
+void print_vector(Kokkos::View<T*> a, Kokkos::View<U*> indices) {
+    // deep copy to host
+    Kokkos::View<T*, Kokkos::HostSpace> host_a("host_a", a.extent(0));
+    Kokkos::View<U*, Kokkos::HostSpace> host_indices("host_indices", indices.extent(0));
 
-    // Print deep copy
-    for (std::uint32_t i = 0; i < view.extent(0); i++) {
-        std::cout << '(' << a[host_view(i)].first << " " << a[host_view(i)].second << ')';
+    Kokkos::deep_copy(host_a, a);
+    Kokkos::deep_copy(host_indices, indices);
 
-        if (i < view.extent(0) - 1) {
+    // print deep copy
+    for (std::uint32_t i = 0; i < a.extent(0); i++) {
+        std::cout << '(' << host_a[host_indices[i]].first << " " << host_a[host_indices[i]].second << ')';
+
+        if (i < a.extent(0) - 1) {
             std::cout << ", ";
         } else {
             std::cout << std::endl;
@@ -35,11 +38,10 @@ int main(int argc, char **argv)
 
     Kokkos::ScopeGuard kokkos;
 
-    std::vector<std::pair<std::uint32_t, std::uint32_t>> a(NUMBER_OF_PAIRS);
-
-    for (std::uint32_t i = 0; i < NUMBER_OF_PAIRS; i++) {
-        a[i] = std::pair<std::uint32_t, std::uint32_t>((NUMBER_OF_PAIRS - 1 - i) / PAIR_GROUPS, (i % PAIR_GROUPS) + 1);
-    }
+    Kokkos::View<Kokkos::pair<std::uint32_t, std::uint32_t>*> a("a", NUMBER_OF_PAIRS);
+    Kokkos::parallel_for("Init", NUMBER_OF_PAIRS, KOKKOS_LAMBDA (const std::uint32_t i) {
+        a(i) = Kokkos::pair<std::uint32_t, std::uint32_t>((NUMBER_OF_PAIRS - 1 - i) / PAIR_GROUPS, (i % PAIR_GROUPS) + 1);
+    });
 
     // Kokkos generate index array
     Kokkos::View<std::uint32_t*> indices("b", NUMBER_OF_PAIRS); // also known as a permutation array
@@ -54,7 +56,7 @@ int main(int argc, char **argv)
 
     //=== Kokkos nested sort with custom comparator ===
     Kokkos::parallel_for("Sort", policy, KOKKOS_LAMBDA (const Kokkos::TeamPolicy<>::member_type& team) {
-        Kokkos::Experimental::sort_team(team, indices, [&a](std::uint32_t i, std::uint32_t j) {
+        Kokkos::Experimental::sort_team(team, indices, [a](std::uint32_t i, std::uint32_t j) {
             // compare pairs by key, the by index
             if (a[i].first == a[j].first) {
                 // Since the indices are sorted, we can just compare them
